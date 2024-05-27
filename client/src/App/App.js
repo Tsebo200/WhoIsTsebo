10efb33e10da840f96a0fd7f1b671c2776ca934b
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-05-27 16:55:32 +0200

    sorting files

---
+++ client/src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Routes>
+       {/* <Route path="/" element={<Dashboard/>}/> */}
+       </Routes> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
