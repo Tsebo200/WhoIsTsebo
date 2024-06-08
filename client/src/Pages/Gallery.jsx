ad45af70f148de063dceb627b41a710f947c6cf4
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-06-08 18:48:05 +0200

    Tried Slideshow in Gallery

--- client/src/Pages/Gallery.jsx
+++ client/src/Pages/Gallery.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Logo from "../Components/Logo/Logo";
 import styles from "../Pages/Gallery.module.scss";
 import NavBar from "../Components/NavBar/NavBar";
+import Example from "../Components/Example/Example";
 
 function Gallery() {
+
+
   return (
     <>
       <NavBar />
@@ -12,16 +15,23 @@ function Gallery() {
         <p className={styles.heading}>Gallery</p>
         <hr className={styles.horLine}></hr>
       </div>
+
+
+
       <div className={styles.contentContainer}>
         <p className={styles.introText}>Feel Free To Hover For More Info</p>
+        <Example/>
+{/* 
         <div className={styles.imgOne}></div>
         <div className={styles.imgTwo}></div>
         <div className={styles.imgThree}></div>
         <div className={styles.chevronContainer}>
-          <div className={styles.chevronSectionOne}></div>
+          <div className={styles.chevronSectionOne}>
+            
+          </div>
           <div className={styles.chevronSectionTwo}></div>
           <div className={styles.chevronSectionThree}></div>
-        </div>
+        </div> */}
       </div>
     </>
   );
