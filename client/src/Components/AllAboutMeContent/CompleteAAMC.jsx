595c2624920e56be6d29ae41fec9fef53d1de361
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2024-11-03 22:05:16 +0200

    move package.json

--- client/src/Components/AllAboutMeContent/CompleteAAMC.jsx
+++ client/src/Components/AllAboutMeContent/CompleteAAMC.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import AllAboutMe from '../AllAboutMeContent/Heading';
 import Body from '../AllAboutMeContent/Body';
+// import App from '../textslider/textslide';
 
 function CompleteAAMC() {
   return (
     <div>
-        <AllAboutMe/>
-        <Body/>
-         </div>
+      <AllAboutMe />
+      <Body />
+      {/* <App/> */}
+    </div>
   )
 }
 
--- client/src/Components/SlideShow/data/images.jsx
+++ client/src/Components/SlideShow/data/images.jsx
@@ -3,6 +3,7 @@ export default [
   "https://i.imgur.com/UArpWAn.png",
   "https://i.imgur.com/2CgnHoi.png",
   "https://i.imgur.com/maZsnL4.png",
+
     // "https://cdn.pixabay.com/photo/2014/05/03/00/56/summerfield-336672_960_720.jpg",
     // "https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg"
   ];
